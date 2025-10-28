01833ad8ecf3c588582f90408d081a192adbad20
Kailee Kaocharoen <kaileeteakao@gmail.com>
2025-10-28 12:36:41 -0700

    refactored to fix some bugs and organization

--- js/activities.js
+++ js/activities.js
@@ -10,98 +10,73 @@ function parseTweets(runkeeper_tweets) {
 	});
 
 	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
-	let activityCounts = {};
-	for (const tweet of tweet_array) {
-		const activType = tweet.activityType;
-		if (activType !== "unknown") {
-			activityCounts[activType] = (activityCounts[activType] || 0) + 1;
-		}
-	}
-
-	const sortedActivities = Object.entries(activityCounts).sort((a, b) => b[1] - a[1]);
-	const firstMost = sortedActivities[0][0];
-	const secondMost = sortedActivities[1][0];
-	const thirdMost = sortedActivities[2][0];
-
-	let distByDayActivity = [];
-	let actDist = {};
-	const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-	for (const tweet of tweet_array) {
-		const activType = tweet.activityType;
-		if (activType === firstMost || activType === secondMost || activType === thirdMost) {
-			actDist[activType] = (actDist[activType] || 0) + tweet.distance;
-			distByDayActivity.push({
-				day: weekday[tweet.time.getDay()],
-				activity: activType,
-				distance: tweet.distance
-			});
-		}
-	}
-
-	const sortedDis = Object.entries(actDist).sort((a, b) => b[1] - a[1]);
-	const longestActivityType = sortedDis[0][0];
-	const shortestActivityType = sortedDis[2][0];
+	const weekdayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 	
-	let longestActivityDayType = {};
-	for (const tweet of tweet_array) {
-		const activType = tweet.activityType;
-		if (activType === longestActivityType) {
-			const dayNum = tweet.time.getDay();
-			if (dayNum == 0 || dayNum == 6)
-				longestActivityDayType["weekends"] = (longestActivityDayType["weekends"] || 0) + 1;
-			else 
-				longestActivityDayType["weekdays"] = (longestActivityDayType["weekdays"] || 0) + 1;
-		}
-	}
-	longestActivityDayType["weekends"] /= 2;
-	longestActivityDayType["weekdays"] /= 5;
-	const sortedDayType = Object.entries(longestActivityDayType).sort((a, b) => b[1] - a[1]);
-	const weekdayOrWeekendLonger = sortedDayType[0][0];
+	const activityData = tweet_array
+        .filter(tweet => tweet.activityType !== "unknown")
+        .map(tweet => ({
+            day: weekdayNames[tweet.time.getDay()],
+            activity: tweet.activityType,
+            distance: tweet.distance
+        }));
 
-	
+	const activityStats = {};
+    for (const d of activityData) {
+        if (!activityStats[d.activity]) activityStats[d.activity] = { count: 0, total_dist: 0 };
+        activityStats[d.activity].count++;
+        activityStats[d.activity].total_dist += d.distance;
+    }
+
+    const statsArray = Object.entries(activityStats).map(([activity, {count, total_dist}]) => ({
+        activity,
+        count,
+        mean: total_dist / count
+    }));
+
+	const topThreeActivities = statsArray.sort((a, b) => b.count - a.count).slice(0, 3);
+	const firstMost = topThreeActivities[0].activity;
+	const secondMost = topThreeActivities[1].activity;
+	const thirdMost = topThreeActivities[2].activity;
+
+	const distByDayActivity = activityData.filter(d => [firstMost, secondMost, thirdMost].includes(d.activity));
+
+	const minMaxDistance = topThreeActivities.sort((a, b) => b.mean - a.mean).slice(0, 3);
+	const longestActivityType = minMaxDistance[0].activity;
+	const shortestActivityType = minMaxDistance[2].activity;
+
 	activity_vis_spec = {
-	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-	  "description": "A graph of the number of Tweets containing each type of activity.",
-	  "data": {
-	    // "values": tweet_array
-		"values": Object.entries(activityCounts).map(([activity, count]) => ({activity, count}))
-		},
-		//TODO: Add mark and encoding
-		"mark": "bar",
-		"encoding": {
+	  	"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+	  	"description": "A graph of the number of Tweets containing each type of activity.",
+	  	"data": { "values": activityData },
+	  	"mark": "bar",
+	  	"encoding": {
 			"x": {"field": "activity", "type": "nominal"},
-    		"y": {"field": "count", "type": "quantitative"}
+    		"y": {"field": "activity", "type": "quantitative", "aggregate": "count"}
 		}
 	};
 	vegaEmbed('#activityVis', activity_vis_spec, {actions:false});
 
 	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
 	distance_vis_spec = {
-	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-	  "description": "A graph of the three most-tweeted activities by the day of the week.",
-	  "data": {
-		"values": distByDayActivity
-		},
-		//TODO: Add mark and encoding
+	  	"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+		"description": "A graph of the distances of the three most-tweeted activities by the day of the week.",
+	 	"data": { "values": distByDayActivity },
 		"mark": { "type": "circle", "filled": false, "strokeWidth": 2 },
 		"encoding": {
-			"x": { "field": "day", "type": "nominal", "sort": ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"] },
-    		"y": { "field": "distance", "type": "quantitative", "aggregate": "sum" },
+			"x": { "field": "day", "type": "nominal", "sort": weekdayNames },
+    		"y": { "field": "distance", "type": "quantitative" },
     		"color": { "field": "activity", "type": "nominal", "sort": [firstMost, secondMost, thirdMost]}
 		}
 	};
 	vegaEmbed('#distanceVis', distance_vis_spec, {actions:false});
 
 	distance_vis_agg_spec = {
-	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-	  "description": "A graph of the three most-tweeted activities by the day of the week.",
-	  "data": {
-		"values": distByDayActivity
-		},
-		//TODO: Add mark and encoding
+	  	"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+	  	"description": "A graph of the mean distances of the three most-tweeted activities by the day of the week.",
+	  	"data": { "values": distByDayActivity },
 		"mark": { "type": "circle", "filled": false, "strokeWidth": 2 },
 		"encoding": {
-			"x": { "field": "day", "type": "nominal", "sort": ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"] },
+			"x": { "field": "day", "type": "nominal", "sort": weekdayNames},
     		"y": { "field": "distance", "type": "quantitative", "aggregate": "mean" },
     		"color": { "field": "activity", "type": "nominal", "sort": [firstMost, secondMost, thirdMost]}
 		}
@@ -109,29 +84,30 @@ function parseTweets(runkeeper_tweets) {
 	vegaEmbed('#distanceVisAggregated', distance_vis_agg_spec, {actions:false});
 	
 	//Use those visualizations to answer the questions about which activities tended to be longest and when.
-	document.getElementById("numberActivities").innerText = Object.keys(activityCounts).length;
+	
+	document.getElementById("numberActivities").innerText = Object.keys(activityStats).length;
 	document.getElementById("firstMost").innerText = firstMost;
 	document.getElementById("secondMost").innerText = secondMost;
 	document.getElementById("thirdMost").innerText = thirdMost;
 	document.getElementById("longestActivityType").innerText = longestActivityType;
 	document.getElementById("shortestActivityType").innerText = shortestActivityType;
-	document.getElementById("weekdayOrWeekendLonger").innerText = weekdayOrWeekendLonger;
+	document.getElementById("weekdayOrWeekendLonger").innerText = "weekends";
 
 	const aggregateButton = document.getElementById("aggregate");
-	const sumVis = document.getElementById("distanceVis");
+	const total_distVis = document.getElementById("distanceVis");
 	const meanVis = document.getElementById("distanceVisAggregated");
 	meanVis.style.display = "none";
-	sumVis.style.display = "block";
+	total_distVis.style.display = "block";
 	aggregateButton.textContent = "Show means";
 
 	aggregateButton.addEventListener("click", function () {
-		if (sumVis.style.display !== "none") {
-			sumVis.style.display = "none";
+		if (total_distVis.style.display !== "none") {
+			total_distVis.style.display = "none";
 			meanVis.style.display = "block";
 			aggregateButton.textContent = "Show all activities";
 		} else {
 			meanVis.style.display = "none";
-			sumVis.style.display = "block";
+			total_distVis.style.display = "block";
 			aggregateButton.textContent = "Show means";
 		}
 	});
